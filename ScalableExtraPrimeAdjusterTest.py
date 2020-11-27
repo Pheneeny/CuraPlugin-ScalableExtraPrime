a2b17c52f4050944d57e9f2923cc1bd1e7b26dc2
Pheneeny <pheneeny@gmail.com>
2020-11-27 01:22:41 -0600

    Changed to have separate toggles to enable extra prime on travels with retractions and on travels without retractions
    Merged Funkton's changes to make plugin compatible with newer versions of Cura
    Added new tests
    Fixed Pep8 formatting issues.
    Updated README with new settings

--- ScalableExtraPrimeAdjusterTest.py
+++ ScalableExtraPrimeAdjusterTest.py
@@ -27,12 +27,12 @@ class TestScalablePrimeAdjusterTest(unittest.TestCase):
         self.assertEqual(gcode1, lepa.combine_gcode(lepa.split_gcode(gcode1)))
         self.assertEqual(gcode2, lepa.combine_gcode(lepa.split_gcode(gcode2)))
         self.assertEqual(gcode3, lepa.combine_gcode(lepa.split_gcode(gcode3)))
-        self.assertEqual(gcode2+" ;comment", lepa.combine_gcode(lepa.split_gcode(gcode2), "comment"));
+        self.assertEqual(gcode2+" ;comment", lepa.combine_gcode(lepa.split_gcode(gcode2), "comment"))
 
     def test_get_point_from_split(self):
-        self.assertEqual(lepa.Point(82.559, 142.583), lepa.get_point_from_split(g1split));
-        self.assertEqual(None, lepa.get_point_from_split(g2split));
-        self.assertEqual(lepa.Point(83.64, 142.561), lepa.get_point_from_split(g3split));
+        self.assertEqual(lepa.Point(82.559, 142.583), lepa.get_point_from_split(g1split))
+        self.assertEqual(None, lepa.get_point_from_split(g2split))
+        self.assertEqual(lepa.Point(83.64, 142.561), lepa.get_point_from_split(g3split))
 
     def test_get_e_from_split(self):
         self.assertEqual(510.05313, lepa.get_e_from_split(g1split))
@@ -74,6 +74,14 @@ class TestScalablePrimeAdjusterTest(unittest.TestCase):
         self.assertEqual(None, lepa.get_e_from_split(adjusted3))
         adjusted4 = lepa.set_e_in_split(g4split, 500)
         self.assertEqual(None, lepa.get_e_from_split(adjusted4))
+
+    def test_should_add_extra_filament(self):
+        self.assertTrue(lepa.should_add_extra_filament(5, 5, True, True))
+        self.assertTrue(lepa.should_add_extra_filament(5, 5, True, False))
+        self.assertTrue(lepa.should_add_extra_filament(5, 0, True, True))
+        self.assertTrue(lepa.should_add_extra_filament(5, 0, False, True))
+        self.assertFalse(lepa.should_add_extra_filament(5, 5, False, True))
+        self.assertFalse(lepa.should_add_extra_filament(5, 0, True, False))
 
     def test_parse_gcode_with_retraction(self):
         gcode = '''G1 X10.00 Y0.00 E2.00
@@ -157,7 +165,7 @@ G0 F7200 X0.00 Y10.00
 G0 F7200 X0.00 Y0.00
 G1 X10.00 Y0.00 E10.0
 '''
-        output = lepa.parse_and_adjust_gcode(["","",gcode, ""], 0, 200, 0, 2, False)
+        output = lepa.parse_and_adjust_gcode(["","",gcode, ""], 0, 200, 0, 2, True, False)
         output = output[2]
         self.assertEqual(expected_output, output)
 
@@ -187,7 +195,7 @@ G0 F7200 X0.00 Y10.0
 G0 F7200 X0.00 Y0.0
 G1 X10.00 Y0.00 E10.2
 '''
-        output = lepa.parse_and_adjust_gcode(["","",gcode, ""], 0, 200, 0, 2, False)
+        output = lepa.parse_and_adjust_gcode(["","",gcode, ""], 0, 200, 0, 2, True, False)
         output = output[2]
         self.assertEqual(expected_output, output)
 
@@ -234,7 +242,7 @@ G1 X10.00 Y0.00 E6.2
         self.assertEqual(point, lepa.Point(1, 280))
 
     def test_parse_gcode_with_G91_last_layer(self):
-        self.maxDiff = None;
+        self.maxDiff = None
         gcode = '''G1 X10.00 Y0.00 E2.00
 G1 X10.000 Y10.00 E4.00
 G1 F1500 E3.5
